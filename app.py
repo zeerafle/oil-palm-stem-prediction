5b5d93bc3ff234011a4d7968ca0aab155e12ba8b
Sam Fareez <vsefareez@gmail.com>
2023-11-25 22:53:34 +0800

    add error handling for predicting empty file

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect
 from werkzeug.utils import secure_filename
 
 from utils import predict
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app.secret_key = os.urandom(24)
 
 
 @app.route('/')
@@ -20,10 +21,16 @@ def predict_image_file():
         f = request.files
         if 'file' not in f:
             return 'No file part', 400
-        uploaded_path = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f['file'].filename))
-        f['file'].save(uploaded_path)
-        prediction = predict(uploaded_path)
-        return render_template('index.html', prediction=prediction, image_path=uploaded_path)
+        if f['file'].filename == '':
+            prediction = ''
+            uploaded_path = ''
+        else:
+            uploaded_path = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f['file'].filename))
+            f['file'].save(uploaded_path)
+            prediction = predict(uploaded_path)
+        flash(prediction)
+        flash(uploaded_path)
+        return redirect('/')
 
 
 if __name__ == '__main__':
